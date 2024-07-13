2c3c911999f4c1902912241b3d8b7739b4e83f82
sudeepaks <92206271+sudeepaks@users.noreply.github.com>
2024-07-13 14:34:08 +0530

    asynch & await

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+       })
+       
+       app.listen(process.env.PORT, () => {
+        console.log(`App is listening on ${process.env.PORT}`);
+       })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+    */
