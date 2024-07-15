87d4eb021b05dca31199587eea3ff0425461dbc1
sudeepaks <92206271+sudeepaks@users.noreply.github.com>
2024-07-15 11:53:45 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`0 Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONO db connection failed !!", err)
+})
 
 
 
