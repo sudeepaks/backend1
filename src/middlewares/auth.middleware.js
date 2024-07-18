23903c77f7222fe2590dc868ba0578361993ba8d
sudeepaks <92206271+sudeepaks@users.noreply.github.com>
2024-07-18 11:59:13 +0530

    access TOken refresh token

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import { asyncHandler } from "../utils/asyncHandler"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { jwt } from "jsonwebtoken"
+import { User } from "../models/user.model.js"
+
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+   try {
+     const token = req.cookies?.accessToken || req.header
+     ("Authorization")?.replace("Bearer", "")
+ 
+     if (!token) {
+         throw new ApiError(401, "unathotized access")
+     }
+ 
+     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+ 
+     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+ 
+     if(!user) {
+         // TODO: discss abot frontend
+         throw new ApiError(401, "Invalid Access Token")
+
+         req.user = user;
+         next()
+
+   }
+}
+   catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token")
+   }
+    
+
+
+
+})
